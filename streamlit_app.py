33bca6b38ec2684f1d5ed785f46dd9f5529da9be
Gal Moore <gal@galmoore.co.uk>
2023-06-29 16:30:42 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -127,3 +127,45 @@ example_dict_outcome = {
 
 # Initialize a new column 'Messages' with dtype object
 df['Messages'] = pd.Series(dtype=object)
+
+# Iterate through the DataFrame
+for index, row in df.iterrows():
+    print("collecting text from document in df and sending query to chatgpt ", index)
+    document_text = row['Text']
+
+    # Ask ChatGPT for dict of results for each document
+    chat_response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": f"You are an israeli nurse in ICU with 20 years experience. When I give you a text string that includes a lab report like this: {example_document}, return a dictionary with the lab results like this: {example_dict_outcome}"},
+            {"role": "user", \
+             "content": f"{document_text}"}
+        ]
+    )
+
+    # Extract the content field
+    messages = chat_response["choices"][0]["message"]["content"]
+    # Replace single quotes with double quotes
+    messages = messages.replace("'", '"')
+    # Convert string representation of dictionary back to dictionary
+    print("messages ",messages)
+    print("index ", index)
+
+    try:
+        messages_dict = json.loads(messages)
+    except json.JSONDecodeError as e:
+        print(f"Unable to parse message into a dictionary: {e}")
+        continue
+
+    # Convert dictionary into a string and update the corresponding column in the DataFrame
+    df.at[index, 'Messages'] = json.dumps(messages_dict)
+
+    # # Update the corresponding column in the DataFrame
+    # df.at[index, 'Messages'] = messages_dict
+
+
+
+
+
+
+

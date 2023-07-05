c1bf30408f8a904b9826b6a05344493eb61e0a54
Gal Moore <gal@galmoore.co.uk>
2023-07-05 18:00:42 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -150,7 +150,7 @@ for index, row in df.iterrows():
 
     # Ask ChatGPT for dict of results for each document
     chat_response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
+        model="gpt-3.5-turbo-16k",
         messages=[
             {"role": "system", "content": f"You are an israeli nurse in ICU with 20 years experience. When I give you a text string that includes a lab report like this: {example_document}, return a dictionary with the lab results like this: {example_dict_outcome}"},
             {"role": "user", \

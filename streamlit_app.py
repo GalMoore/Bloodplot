12cb086b5418fa039f162ddf5f0f00a8f5666f3b
Gal Moore <gal@galmoore.co.uk>
2023-07-05 16:16:04 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ Our aim? Making medical reports accessible without a medicine degree. After all,
 
 st.markdown(f'<div style="text-align: justify;">{my_intro_text}</div>', unsafe_allow_html=True)
 st.markdown("\n")
-st.markdown("**:blue[Upload your PDF files on the left sidebar]** and watch the graphs populate below.")
+st.markdown("**:pink[Upload your PDF files on the left sidebar]** and watch the graphs populate below.")
 
 # Loop over each uploaded file
 if uploaded_files:

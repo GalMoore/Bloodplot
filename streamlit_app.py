b6bcb317bac12a83503299301c31c2504d1f7f36
Gal Moore <gal@galmoore.co.uk>
2023-07-05 18:21:50 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -143,6 +143,23 @@ example_dict_outcome = {
 # Initialize a new column 'Messages' with dtype object
 df['Messages'] = pd.Series(dtype=object)
 
+# CHECK WORD COUNT FOR EACH DOCUMENT
+df['word_count'] = df['Text'].apply(lambda x: len(str(x).split()))
+
+
+### CHECK HOW MANY TOKENS IN THE LONGEST TEXT AND PRINT / write to screen 
+def num_tokens_from_string(string: str, encoding_name: str) -> int:
+    """Returns the number of tokens in a text string."""
+    # encoding = tiktoken.get_encoding(encoding_name)
+    encoding = tiktoken.encoding_for_model('gpt-3.5-turbo')
+    num_tokens = len(encoding.encode(string))
+    return num_tokens
+df['token_count'] = df['Text'].apply(lambda x: num_tokens_from_string(x, encoding))
+
+st.markdown(df)
+
+
+
 ### ONE SHOT LEARNING 
 # Iterate through the DataFrame
 for index, row in df.iterrows():

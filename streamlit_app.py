b56ed067f817f7b68bbe226173d1fe9d806fdba1
Gal Moore <gal@galmoore.co.uk>
2023-07-12 11:34:54 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,8 @@ import plotly.express as px
 import matplotlib.pyplot as plt
 import re
 
-st.markdown('<h1 style="font-size:40px;">:sun_behind_cloud::microscope: CloudLab</h1>', unsafe_allow_html=True)
+# st.markdown('<h1 style="font-size:40px;">:sun_behind_cloud::microscope: CloudLab</h1>', unsafe_allow_html=True)
+st.title(":sun_behind_cloud::microscope: CloudLab ") # : Your Health Vault 
 
 
 with open( "style.css" ) as css:
